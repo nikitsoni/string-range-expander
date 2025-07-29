c3329f48a99f89362bbc84fb93ae3a63a3313331
Nikit Soni <nikitsoni@Nikits-MacBook-Air.local>
2025-07-29 22:37:50 +0530

    setting up the project structure

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def main():
+    try:
+        input_str = input("Enter string with numbers and ranges: ")
+        print(input_str)
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
